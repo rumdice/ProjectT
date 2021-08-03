d9493058b6bdbe8b57bb6b6a6e5d498c960b6ff6
rumdice <rumdice@gmail.com>
2021-08-03 14:44:10 +0900

    add log page

--- admin_svr/routes/log.ts
+++ admin_svr/routes/log.ts
@@ -1,56 +1,56 @@
-// import express = require('express');
-// import { readApiErrorDir, readApiErrorLog } from '../../common/logger';
+import express = require('express');
+import { readApiErrorDir, readApiErrorLog } from '../../common/logger';
 
-// let res_view = 'log';
-// let res_body = {
-//     title: 'log',
-//     logNameList: [""],
-//     logDataList: [""],
-//     idx: 0
-// };
+let res_view = 'log';
+let res_body = {
+    title: 'log',
+    logNameList: [""],
+    logDataList: [""],
+    idx: 0
+};
 
-// let viewLogRes = 'logRes';
+let viewLogRes = 'logRes';
 
-// const router = express.Router();
-// router.get('/', (req, res) => {
-//     let logDir = readApiErrorDir();
-//     let logNames: string[] = [];
+const router = express.Router();
+router.get('/', (req, res) => {
+    let logDir = readApiErrorDir();
+    let logNames: string[] = [];
 
-//     logDir.forEach(e => {
-//         let filetype = e.split(".");
-//         if (filetype[1] === 'log') {
-//             logNames.push(filetype[0]);
-//         }
-//     });
+    logDir.forEach(e => {
+        let filetype = e.split(".");
+        if (filetype[1] === 'log') {
+            logNames.push(filetype[0]);
+        }
+    });
 
-//     res_body.logNameList = logNames;
-//     res.render(res_view, res_body);
-// });
+    res_body.logNameList = logNames;
+    res.render(res_view, res_body);
+});
 
-// router.post('/detail', (req, res) => {
+router.post('/detail', (req, res) => {
     
-//     let logDir = readApiErrorDir();
-//     let logNames: string[] = [];
-//     let logDatas: string[] = [];
+    let logDir = readApiErrorDir();
+    let logNames: string[] = [];
+    let logDatas: string[] = [];
     
-//     logDir.forEach(e => {
-//         let filetype = e.split(".");
-//         if (filetype[1] === 'log') {
-//             logNames.push(filetype[0]);
-//         }
-//     });
+    logDir.forEach(e => {
+        let filetype = e.split(".");
+        if (filetype[1] === 'log') {
+            logNames.push(filetype[0]);
+        }
+    });
     
-//     logNames.forEach(e => {
-//         let logData = readApiErrorLog(e).toString();
-//         logData = logData.replace(/\r/g, "");
+    logNames.forEach(e => {
+        let logData = readApiErrorLog(e).toString();
+        logData = logData.replace(/\r/g, "");
 
-//         logDatas.push(logData);
-//     });
+        logDatas.push(logData);
+    });
 
-//     res_body.idx = req.body.bidx;
-//     res_body.logNameList = logNames;
-//     res_body.logDataList = logDatas;
-//     res.render(viewLogRes, res_body);  // post 결과를 해당 페이지의 modal로 표현하고 싶지만, 일단 별도 페이지에 render.
-// });
+    res_body.idx = req.body.bidx;
+    res_body.logNameList = logNames;
+    res_body.logDataList = logDatas;
+    res.render(viewLogRes, res_body);  // post 결과를 해당 페이지의 modal로 표현하고 싶지만, 일단 별도 페이지에 render.
+});
 
-// export default router;
+export default router;
