183662e3f4a266d797ad3e137782e32a435ca58e
rumdice <rumdice@gmail.com>
2021-08-03 13:35:19 +0900

    bash 스크립트 오류 수정

--- script/start_admin_server.sh
+++ script/start_admin_server.sh
@@ -3,6 +3,5 @@ cd ~/ProjectT/script/
 sh refresh_admin_resource.sh
 
 # Reload OR Start 서버
-cd ~/ProjectT/
 pm2 reload rumdice-admin starter_admin.json || pm2 start starter_admin.json --env dev
 pm2 list
