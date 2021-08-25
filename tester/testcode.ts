dca48db034c570d9191a290887165a3a16ca7105
rumdice <rumdice@gmail.com>
2021-08-25 18:50:19 +0900

    test코드

--- tester/testcode.ts
+++ tester/testcode.ts
@@ -12,57 +12,53 @@
 // 기본 타입(bool, number, bigint, string, symbol)과 달리 객채를 const로 선언해도 더 좁은 타입으로 추론하지 않음.
 // 타입 스크립트도 자바스크립트와 마찬가지로 객체를 만든 후 필드값을 바꿀 수 있다는 사실을 알기 때문
 
-import * as a from 'async'
 
 
-const doWork = async () => {
-    // 작업 3개가 동시에 시작됨 (시작 순서는 보장)
-    // await func1()
-    // dawait func2()
-    // await func3()
+// const main = async () => {
+//     // 작업 3개가 동시에 시작됨 (시작 순서는 보장)
+//     // await func1()
+//     // dawait func2()
+//     // await func3()
 
 
-    // 위에랑 같음
-    // await Promise.all([func1(), func2(), func3()])
+//     // 위에랑 같음
+//     // await Promise.all([func1(), func2(), func3()])
 
-    // 같음
-    // await func1().then(func2)
+//     // 같음
+//     // await func1().then(func2)
 
-    // 작업 함수에서 리턴하는 경우
-    // const r1 = await func1()
-    // const r2 = await func2()
-    // console.log(r1)
-    // console.log(r2)
-
-
-    // 하고자 하는 것 func1함수가 완료되면 func2,3 실행
-    a.eachSeries([func1, func2, func3], async (err, result) => { console.log(err, result) })
-}
-
-async function func1(cb: any) {
-    console.log("func1 start")
-    setTimeout(() => outlog("func1"), 3000)
-    cb(null, "1")
-}
-
-async function func2(cb: any) {
-    console.log("func2 start")
-    setTimeout(() => outlog("func2"), 2000)
-    cb(null, "2")
-}
-
-async function func3(cb: any) {
-    console.log("func3 start")
-    setTimeout(() => outlog("func3"), 1000)
-    cb(null, "2")
-}
-
-function outlog(logstr: string) {
-    console.log(`${logstr} done`)
-}
+//     // 작업 함수에서 리턴하는 경우
+//     // const r1 = await func1()
+//     // const r2 = await func2()
+//     // console.log(r1)
+//     // console.log(r2)
 
 
-doWork()
+//     // 하고자 하는 것 func1함수가 완료되면 func2,3 실행
+//     const fu1 = await func1()
+// }
+
+// async function func1() {
+//     console.log("func1 start")
+//     setTimeout(() 3000)
+// }
+
+// async function func2() {
+//     console.log("func2 start")
+//     setTimeout(() => 2000)
+// }
+
+// async function func3() {
+//     console.log("func3 start")
+//     setTimeout(()=> 1000)
+// }
+
+// const taskDone = (logstr: string) => {
+//     console.log(`${logstr} done`)
+// }
+
+
+// main()
 
 
 
@@ -81,3 +77,40 @@ doWork()
 // )
 
 // eachSeries()
+
+
+
+const random = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1)) + min
+
+const installOS = () => asyncTask("Install OS")
+
+const deploySoftware = () => asyncTask("Deploy Software")
+
+const runTests = () => asyncTask("Run Tests")
+
+const taskDone = (name: unknown) => console.log(`Completed async "${name}"`)
+
+const asyncTask = (name: unknown) => {
+    console.log(`Started async "${name}"...`)
+
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => resolve(name), random(1, 3) * 1000)
+    })
+
+    console.log(`Returning from async "${name}"`)
+
+    return promise
+}
+
+const main = async () => {
+    const installOSResult = await installOS()
+    taskDone(installOSResult)
+
+    const deploySoftwareResult = await deploySoftware()
+    taskDone(deploySoftwareResult)
+
+    const runTestsResult = await runTests()
+    taskDone(runTestsResult)
+}
+
+main()
