57d29f546a6a37c4e6f7a75f3d741cb212fad1ac
rumdice <rumdice@gmail.com>
2021-12-09 08:18:15 +0900

    같은 작업자 PC에서 docker 를 구분해서 실행 시키기 위해 설정 파일 및 port 분리 - 각 프로젝트마다 DB를 완전히 구분해서 실행

---
+++ script/docker_projectT/Init_docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# 사용자 pc에 docker desktop 설치 여부 확인
+# 같은 PC에 같은 port를 중복해서 쓸 수 없어서 일반적인 사용 포트와 조금 다름
+# mysql : (3306 -> 3305)
+# redis : (6379 -> 6378)
+
+# 옵션 설명 
+# -f (강제 실행)
+# up (컨테이너 생성 실행)
+# -d (deamon 백그라운드에서 실행)
+# --build (이미지 빌드)
+
+# redis
+docker-compose -f redis_docker_compose.yml up -d --build
+
+# mariadb
+docker-compose -f mariadb_docker_compose.yml up -d --build
