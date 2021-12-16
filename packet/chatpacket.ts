0b739cfbd5d60d1fd752f01bc88e6c35cdaeb4d3
rumdice <rumdice@gmail.com>
2021-12-16 13:30:57 +0900

    채팅 패킷 수정 및 개선

--- packet/chatpacket.ts
+++ packet/chatpacket.ts
@@ -1,81 +1,29 @@
 import { ErrorCode } from "./errorCode";
 
-export interface chatLog {
-    userUid: string;
-    msg: string;
-    recv_time: Date;
-}
-
-export interface RequestBodyLogin extends RequestBody {
+export interface BaseRequest {
     userUid: string;
 }
 
-export interface RequestBodyChat extends RequestBody {
-    userUid: string;
-    chatMsg: string;
-}
-
-export interface RequestBodyChatLog extends RequestBody {
-    userUid: string;
-}
-
-export interface ResponseBodyLogin extends ResponseBody {
-    userUid: string;
-}
-
-export interface ResponseBodyChat extends ResponseBody {
-    userUid: string;
-    chatMsg: string;
-}
-
-export interface ResponseBodyChatLog extends ResponseBody {
-    chatLogList: chatLog[];
-}
-
-export interface RequestLogin extends RequestPacket<RequestBodyLogin> {
-    header: string;
-}
-
-export interface RequestChat extends RequestPacket<RequestBodyChat> {
-    header: string;
-}
-
-export interface RequestChatLog extends RequestPacket<RequestBodyChatLog> {
-    header: string;
-}
-
-export interface ResponseLogin extends ResponsePacket<ResponseBodyLogin> {
-
-}
-
-export interface ResponseChat extends ResponsePacket<ResponseBodyChat> {
-
-}
-
-export interface ResponseChatLog extends ResponsePacket<ResponseBodyChatLog> {
-
-}
-
-export interface RequestBody {
-
-}
-
-export interface ResponseBody {
+export interface BaseResponse {
     error: ErrorCode;
+    message: string;
 }
 
-export interface RequestPacketBase {
-    header: string;
+export interface RequestChatLogin extends BaseRequest {
+    uRL: string;
+    userName: string;
 }
 
-export interface ResponsePacketBase {
-    header: string;
+export interface ResponseChatLogin extends BaseResponse {
+
 }
 
-export interface ResponsePacket<T> extends ResponsePacketBase {
-    body: T;
+export interface RequestNormalChat extends BaseRequest {
+    userUid: string;
+    sendMessage: string;
 }
 
-export interface RequestPacket<T> extends RequestPacketBase {
-    body: T;
+export interface RequestBodyChat extends BaseResponse {
+    userUid: string;
+    recvMessage: string;
 }
